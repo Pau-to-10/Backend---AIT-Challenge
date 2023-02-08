59a0d994a30794ddb5c4729f6539675e3965deda
Pau Tomás <tomas10pau@gmail.com>
2023-02-09 00:13:48 +0100

    feat: generate token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,46 @@
 import { User } from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = new User({ email, password });
+    let user = await User.findOne({ email });
+    if (user) throw { code: 11000 };
+
+    user = new User({ email, password });
     await user.save();
 
     //jwt token
 
-    return res.json({ ok: "register" });
+    return res.status(201).json({ ok: "register" });
   } catch (error) {
     console.log(error);
+    if (error.code === 11000) {
+      return res.status(400).json({ error: "Ya existe este usuario" });
+    }
+    return res.status(500).json({ error: "Error de servidor" });
   }
 };
 
 export const login = async (req, res) => {
-  res.json({ ok: "login" });
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (!user) return res.status(403).json({ error: "No existe este usuario" });
+
+    const passwordRespond = await user.comparePassword(password);
+    if (!passwordRespond) {
+      return res.status(403).json({ error: "Contraseña incorrecta" });
+    }
+
+    // Generar el token JWT
+    const token = jwt.sign({ uid: user._id }, process.env.JWT_SECRET);
+
+    return res.json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error de servidor" });
+  }
 };
